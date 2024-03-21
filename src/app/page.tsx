87bc1c2fe41a23589dbe3abbe37c9343142a0337
ysoares0209 <yan@westpoint.io>
2024-03-21 11:50:54 -0300

    feat: test page break

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,11 @@ import classes from "@/styles/page.module.css";
 
 const PDFWrapper = dynamic(() => import("../components/PDFWrapper"), { ssr: false });
 const PDFWrapperMemoized = memo(PDFWrapper, (prevProps, nextProps) => {
-  return prevProps.title === nextProps.title && prevProps.subTitle === nextProps.subTitle;
+  return (
+    prevProps.title === nextProps.title &&
+    prevProps.subTitle === nextProps.subTitle &&
+    prevProps.mockedParagraphCount === nextProps.mockedParagraphCount
+  );
 });
 
 export default function Home() {
@@ -14,9 +18,14 @@ export default function Home() {
   const [subtitle, setSubtitle] = useState(
     "This document should not be used to create a tenancy where the initial fixed term is to be for more than three years; you should consult a Solicitor as such an agreement must be created by Deed."
   );
+  const [mockedParagraphsCount, setMockedParagraphsCount] = useState(0);
 
   const [debouncedTitle] = useDebounce(title, 1000);
   const [debouncedSubtitle] = useDebounce(subtitle, 1000);
+
+  const addMockedParagraph = () => {
+    setMockedParagraphsCount(mockedParagraphsCount + 1);
+  };
 
   return (
     <main className={classes.root}>
@@ -32,10 +41,15 @@ export default function Home() {
             <label htmlFor="subtitle">Subtitle</label>
             <input id="subtitle" value={subtitle} onChange={(e) => setSubtitle(e.target.value)} />
           </div>
+          <button onClick={addMockedParagraph}>Add paragraph</button>
         </section>
         {/* LEFT SECTION */}
         <section className={classes.rightSection}>
-          <PDFWrapperMemoized title={debouncedTitle} subTitle={debouncedSubtitle} />
+          <PDFWrapperMemoized
+            title={debouncedTitle}
+            subTitle={debouncedSubtitle}
+            mockedParagraphCount={mockedParagraphsCount}
+          />
         </section>
       </div>
     </main>
--- src/components/PDFWrapper.tsx
+++ src/components/PDFWrapper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import React from "react";
+import React, { useMemo } from "react";
 import { Page, Text, Font, Document, StyleSheet } from "@react-pdf/renderer";
 
 const PDFViewer = dynamic(() => import("@react-pdf/renderer").then((mod) => mod.PDFViewer), {
@@ -56,20 +56,77 @@ const pdfStyles = StyleSheet.create({
 interface Props {
   title: string;
   subTitle: string;
+  mockedParagraphCount: number;
 }
 
-const PDFWrapper = ({ title, subTitle }: Props) => {
+const mockedParagraph = () => (
+  <>
+    <Text>Mocked paragraph</Text>
+    <Text style={pdfStyles.text}>
+      The responsibilities of both the landlord and tenant regarding the upkeep of the premises. The
+      landlord is typically responsible for structural repairs and the maintenance of utilities,
+      while the tenant is expected to keep the property clean and report any issues that require
+      repair.
+    </Text>
+  </>
+);
+
+const PDFWrapper = ({ title, subTitle, mockedParagraphCount }: Props) => {
+  const mockedParagraphs = useMemo(() => {
+    const array = new Array(mockedParagraphCount).fill(mockedParagraph);
+    return array;
+  }, [mockedParagraphCount]);
+
   return (
     <PDFViewer height={"700"} width={"600"} showToolbar={false}>
       <Document pageMode="fullScreen">
         <Page size="A4" style={pdfStyles.body}>
           <Text style={pdfStyles.title}>{title}</Text>
           <Text style={pdfStyles.subtitle}>{subTitle}</Text>
+          <Text>The premises</Text>
+          <Text style={pdfStyles.text}>
+            The premises include all, or any parts of the dwelling-house, gardens, paths, fences,
+            boundaries or other outbuildings which form part of the let. Where the premises form
+            only part of another property (e.g. in a block of flats), the letting includes the use,
+            in common with others, of communal access ways and other similar facilities.
+          </Text>
+          <Text>Binding Date</Text>
+          <Text style={pdfStyles.text}>
+            A tenancy agreement is not, technically, a legally binding contract until it has been
+            “executed” by being Dated, after both parties (or their authorised representatives) have
+            signed; although it might be possible for either party to take legal action against the
+            other if they withdraw prior to this date.
+          </Text>
+          <Text>Landlord</Text>
+          <Text style={pdfStyles.text}>
+            A person or persons who at any relevant time own, or have a formal interest in, the
+            premises that gives them the right to possession of the premises.
+          </Text>
+          <Text>Tenant</Text>
+          <Text style={pdfStyles.text}>
+            A person, or persons, who at any relevant time are entitled to occupy the premises under
+            the terms of this tenancy agreement.
+          </Text>
           <Text
             style={pdfStyles.pageNumber}
             render={({ pageNumber, totalPages }) => `${pageNumber} / ${totalPages}`}
             fixed
           />
+          <Text>Rent</Text>
+          <Text style={pdfStyles.text}>
+            The amount of money paid by the tenant to the landlord, usually on a monthly basis, for
+            the right to occupy the premises. The rent amount is agreed upon at the start of the
+            tenancy and may be subject to increase in accordance with the terms specified in the
+            tenancy agreement or as allowed by law.
+          </Text>
+          <Text>Deposit</Text>
+          <Text style={pdfStyles.text}>
+            A sum of money held by the landlord as security for the performance of any obligations
+            of the tenant, or to cover any damage to the premises beyond normal wear and tear. The
+            deposit must be returned to the tenant at the end of the tenancy, subject to any
+            deductions for damage or unpaid rent.
+          </Text>
+          {mockedParagraphs.map((data) => data())}
         </Page>
       </Document>
     </PDFViewer>
